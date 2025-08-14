8d26ad17e0e7753376210787249aaf2003525869
Jeyakumarthomas <88367696+Jeyakumarthomas@users.noreply.github.com>
2025-08-14 15:58:18 +0530

    Header and 1/3 of hero section created responsive

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,36 +1,51 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Link} from 'react-scroll'
 import Logo from '../assets/Logo.png'
 import './Header.css'
 
+
+
 const Header = () => {
+
+  const [isMenuOpen,setIsMenuOpen] = useState(false);  
+
   return (
-        <header className='flex justify-around items-center text-black h-[84px] bg-white sticky top-0 z-10'>
-
-            {/* logo  */}
-            <a href="#"> <img src={Logo} alt="" className='w-[155px] h-6'/></a>
-
-            {/* group */}
-            <div>
-                <ul className=' flex justify-between text-md h-[24px] w-[588px]'>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="Home" smooth={true} duration={500}>Home</Link></li>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="Service" smooth={true} duration={500}>Service</Link></li>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="Feature" smooth={true} duration={500}>Feature</Link></li>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="Product" smooth={true} duration={500}>Product</Link></li>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="Testimonial" smooth={true} duration={500}>Testimonial</Link></li>
-                <li className="hover:text-gray-600 duration-300 cursor-pointer"><Link to="FAQ" smooth={true} duration={500}>FAQ</Link></li>
+       <header className='flex justify-between items-center py-6 px-8 md:px-32 bg-white '>
+            <a href=""><img src={Logo} alt="Logo" className='w-52 hover:scale-105 transition-all' /></a>
+            
+            <ul className='hidden xl:flex items-center gap-12 font-normal text-base '>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="Service" smooth={true} duration={500}>Service</Link></li>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="Feature" smooth={true} duration={500}>Feature</Link></li>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="Product" smooth={true} duration={500}>Product</Link></li>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="Testimonial" smooth={true} duration={500}>Testimonial</Link></li>
+                <li className='p-3 cursor-pointer hover:text-gray-500 hover:scale-105 transition-all'><Link to="FAQ" smooth={true} duration={500}>FAQ</Link></li>
             </ul>
+
+            <div className='hidden xl:flex items-center justify-center gap-1'>
+                <button className='px-5 py-2.5 rounded-lg text-primary cursor-pointer'>Login</button>
+                <button className='px-5 py-2.5 rounded-lg bg-primary text-white cursor-pointer'>Signup</button>
             </div>
 
-            {/* login & SignUp */}
-            <div className=' flex items-center gap-2 w-[182px] h-[40px]'>
-             
-                    <button className=" py-2.5 px-5 text-sm rounded-md text-primary">Login</button>
-                
-                    <button className=" py-2.5 px-5 text-sm text-white rounded-md bg-primary">Sign up</button>
-                
-            </div>
-        </header>
+            <div className='xl:hidden block text-black text-5xl cursor-pointer' onClick={()=>{setIsMenuOpen(!isMenuOpen)}} >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </div> 
+
+           {isMenuOpen && <div className='absolute xl:hiddden top-24 left-0 w-full bg-green-200 flex flex-col items-center gap-1 pb-2 font-semibold text-normal' >
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="Home" smooth={true} duration={500}>Home</Link></li>
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="Service" smooth={true} duration={500}>Service</Link></li>
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="Feature" smooth={true} duration={500}>Feature</Link></li>
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="Product" smooth={true} duration={500}>Product</Link></li>
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="Testimonial" smooth={true} duration={500}>Testimonial</Link></li>
+                <li className='list-none w-full text-center p-4 transition-all cursor-pointer'><Link to="FAQ" smooth={true} duration={500}>FAQ</Link></li>
+                <div className='flex items-center justify-center gap-1'>
+                    <button className='px-5 py-2.5 rounded-lg text-primary cursor-pointer'>Login</button>
+                    <button className='px-5 py-2.5 rounded-lg bg-primary text-white cursor-pointer'>Signup</button>
+                </div>
+            </div>}
+       </header>
 )
 }
 
--- src/componenets/Homehero.jsx
+++ src/componenets/Homehero.jsx
@@ -3,22 +3,36 @@ import Illustration from '../assets/Illustration.png';
 
 const Homehero = () => {
   return (
-        <div className='flex justify-between w-full bg-silver py-24 px-32'>
+        // <div className='w-full bg-silver py-24 px-32 flex'>
 
-            <div className='flex w-1/2 justify-center'>
-              <div className='flex flex-col justify-center gap-5 '>
-                <h1 className='font-semibold text-[64px] leading-[76px] text-grayHead'>Lessons and insights<br/> <span className='text-primary'>from 8 years</span></h1>
-                <p className='leading-6 text-grayPara'>Where to grow your business as a photographer: site or social media?</p>
-                <button className="bg-primary text-white py-3.5 px-8 rounded-md w-32">Register</button>
-              </div>
-            </div>
+        //     <div className=' w-1/2 flex justify-center'>
+        //       <div className='flex flex-col justify-center gap-5 '>
+        //         <h1 className='text-[64px] font-semibold text-grayHead leading-[76px] '>Lessons and insights<br/> <span className='text-primary'>from 8 years</span></h1>
+        //         <p className=' text-grayPara leading-6'>Where to grow your business as a photographer: site or social media?</p>
+        //         <button className="w-32 py-3.5 px-8 bg-primary text-white rounded-md ">Register</button>
+        //       </div>
+        //     </div>
 
-            <div className='flex justify-center w-1/2 '>
-              <div className='w-[391-px] h-[407px]'>
-                <img src={Illustration} alt="Illustration"  />
-              </div>
-            </div>
-        </div>
+        //     <div className='w-1/2 flex justify-center  '>
+        //       <div className='w-[391-px] h-[407px]'>
+        //         <img src={Illustration} alt="Illustration"  />
+        //       </div>
+        //     </div>
+            
+        // </div>  
+
+        <section className='w-full flex flex-col md:flex-row px-5 py-32 justify-center items-center bg-silver'>
+
+          <div className='md:w-1/2 flex flex-col gap-8'>
+             <h1 className='text-6xl font-semibold text-grayHead leading-16 '>Lessons and insights<br/> <span className='text-primary'>from 8 years</span></h1>
+             <p className=' text-grayPara leading-6'>Where to grow your business as a photographer: site or social media?</p>
+             <button className="w-32 py-3.5 px-8 bg-primary text-white rounded-md ">Register</button>
+          </div>
+        
+          <img src={Illustration} alt="Illustration" className='md:w-1/4' />
+          
+           
+        </section>
   )
 }
 
